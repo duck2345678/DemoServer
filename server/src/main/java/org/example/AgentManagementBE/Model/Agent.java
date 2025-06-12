814ae94f211dd8b0819b58ff20328125690d1c30
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 00:28:22 +0700

    Update Agent.java

--- server/src/main/java/org/example/AgentManagementBE/Model/Agent.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/Agent.java
@@ -8,160 +8,140 @@ import java.util.Date;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-
-public class Agent 
-{
+public class Agent {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "agentID")// mã đại lý
-    private int agentID;
-    @Column(name = "agentName",nullable = false,columnDefinition = "NVARCHAR(255)")
+    @Column(name = "agentID") // mã đại lý
+    private int agentId;
+
+    @Column(name = "agentName", nullable = false, columnDefinition = "NVARCHAR(255)")
     private String agentName;
-    @Column(name = "address",nullable = false,columnDefinition = "NVARCHAR(255)")
+
+    @Column(name = "address", nullable = false, columnDefinition = "NVARCHAR(255)")
     private String address;
-    @Column(name = "phone",nullable = false)
+
+    @Column(name = "phone", nullable = false)
     private String phone;
-    @Column(name = "email",nullable = false)
+
+    @Column(name = "email", nullable = false)
     private String email;
+
     @ManyToOne
     @JoinColumn(name = "agentTypeID") // mã loại đại lý
-    private AgentType agentTypeID;
+    private AgentType agentTypeId;
 
     @ManyToOne
     @JoinColumn(name = "districtID") // mã quận
-    private District districtID;
+    private District districtId;
 
     @Temporal(TemporalType.TIMESTAMP)
     @CreatedDate
     @Column(name = "receptionDate") // ngày tiếp nhận
     private Date receptionDate;
+
     @Column(name = "debtMoney") // số tiền nợ
     private int debtMoney;
 
-    public Agent() 
-    {
-
+    public Agent() {
     }
-    
-    public Agent(int agentID, String agentName)
-    {
-        this.agentID = agentID;
+
+    public Agent(int agentId, String agentName) {
+        this.agentId = agentId;
         this.agentName = agentName;
     }
 
-    public Agent(int agentID, String agentName, String address, String phone, String email, AgentType agentTypeID, District districtID, Date receptionDate, int debtMoney) 
-    {
-        this.agentID = agentID;
+    public Agent(int agentId, String agentName, String address, String phone, String email, AgentType agentTypeId, District districtId, Date receptionDate, int debtMoney) {
+        this.agentId = agentId;
         this.agentName = agentName;
         this.address = address;
         this.phone = phone;
         this.email = email;
-        this.agentTypeID = agentTypeID;
-        this.districtID = districtID;
+        this.agentTypeId = agentTypeId;
+        this.districtId = districtId;
         this.receptionDate = receptionDate;
         this.debtMoney = debtMoney;
     }
 
-    public Agent(String agentName, String address, String phone, String email, AgentType agentTypeID, District districtID, Date receptionDate, int debtMoney) 
-    {
-            this.agentName = agentName;
-            this.address = address;
-            this.phone = phone;
-            this.email = email;
-            this.agentTypeID = agentTypeID;
-            this.districtID = districtID;
-            this.receptionDate = receptionDate;
-            this.debtMoney = debtMoney;
+    public Agent(String agentName, String address, String phone, String email, AgentType agentTypeId, District districtId, Date receptionDate, int debtMoney) {
+        this.agentName = agentName;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.agentTypeId = agentTypeId;
+        this.districtId = districtId;
+        this.receptionDate = receptionDate;
+        this.debtMoney = debtMoney;
     }
 
-    public int getAgentID() 
-    {
-        return agentID;
+    public int getAgentId() {
+        return agentId;
     }
 
-    public void setAgentID(int agentID) 
-    {
-        this.agentID = agentID;
+    public void setAgentId(int agentId) {
+        this.agentId = agentId;
     }
 
-    public String getAgentName() 
-    {
+    public String getAgentName() {
         return agentName;
     }
 
-    public void setAgentName(String agentName) 
-    {
+    public void setAgentName(String agentName) {
         this.agentName = agentName;
     }
 
-    public String getAddress() 
-    {
+    public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) 
-    {
+    public void setAddress(String address) {
         this.address = address;
     }
 
-    public String getPhone() 
-    {
+    public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) 
-    {
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 
-    public String getEmail() 
-    {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) 
-    {
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public AgentType getAgentTypeID() 
-    {
-        return agentTypeID;
+    public AgentType getAgentTypeId() {
+        return agentTypeId;
     }
 
-    public void setAgentTypeID(AgentType agentTypeID) 
-    {
-        this.agentTypeID = agentTypeID;
+    public void setAgentTypeId(AgentType agentTypeId) {
+        this.agentTypeId = agentTypeId;
     }
 
-    public District getDistrictID() 
-    {
-        return districtID;
+    public District getDistrictId() {
+        return districtId;
     }
 
-    public void setDistrictID(District districtID) 
-    {
-        this.districtID = districtID;
+    public void setDistrictId(District districtId) {
+        this.districtId = districtId;
     }
 
-    public Date getReceptionDate() //ngay tiep nhan
-    {
+    public Date getReceptionDate() {
         return receptionDate;
     }
 
-    public void setReceptionDate(Date receptionDate) 
-    {
+    public void setReceptionDate(Date receptionDate) {
         this.receptionDate = receptionDate;
     }
 
-    public int getDebtMoney() 
-    {
+    public int getDebtMoney() {
         return debtMoney;
     }
 
-    public void setDebtMoney(int debtMoney) 
-    {
+    public void setDebtMoney(int debtMoney) {
         this.debtMoney = debtMoney;
     }
-
 }
