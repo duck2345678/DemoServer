4f0894fa030638642d9531a7533b539199b1782e
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 00:59:00 +0700

    Update ImportReceipt.java

--- server/src/main/java/org/example/AgentManagementBE/Model/ImportReceipt.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/ImportReceipt.java
@@ -4,28 +4,29 @@ import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.sql.Date;
-
+import java.util.Date;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class ImportReceipt {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "importReceiptID") // mã phiếu nhập
     private int importReceiptID;
-    @Column(name = "dateReceipt") // ngày lập phiếu
+
+    @Temporal(TemporalType.DATE)
+    @CreatedDate
+    @Column(name = "dateReceipt", nullable = false, updatable = false) // ngày lập phiếu
     private Date dateReceipt;
 
-    @Column(name = "totalPrice",nullable = false) // tổng giá
+    @Column(name = "totalPrice", nullable = false) // tổng giá
     private int totalPrice;
 
-    public ImportReceipt() {        
+    public ImportReceipt() {
     }
 
-
-
-    public ImportReceipt(int importReceiptID,int totalPrice) {
+    public ImportReceipt(int importReceiptID, int totalPrice) {
         this.importReceiptID = importReceiptID;
         this.totalPrice = totalPrice;
     }
@@ -34,16 +35,27 @@ public class ImportReceipt {
         this.totalPrice = totalPrice;
     }
 
-
     public int getImportReceiptID() {
         return importReceiptID;
+    }
+
+    public void setImportReceiptID(int importReceiptID) {
+        this.importReceiptID = importReceiptID;
     }
 
     public Date getDateReceipt() {
         return dateReceipt;
     }
+
+    public void setDateReceipt(Date dateReceipt) {
+        this.dateReceipt = dateReceipt;
+    }
+
     public int getTotalPrice() {
         return totalPrice;
     }
 
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
