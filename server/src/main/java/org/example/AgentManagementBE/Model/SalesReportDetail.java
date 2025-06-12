f5e1d4d1b5bcbe6840ad5dc007c3d2e7870626ba
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 01:42:10 +0700

    Update SalesReportDetail.java

--- server/src/main/java/org/example/AgentManagementBE/Model/SalesReportDetail.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/SalesReportDetail.java
@@ -1,36 +1,40 @@
 package org.example.AgentManagementBE.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+
 import java.io.Serializable;
 
 @Entity
 @IdClass(SalesReportDetail.SalesReportDetailID.class)
-@Table(name = "SalesReportDetail")
-public class SalesReportDetail {
-
+public class SalesReportDetail 
+{
     @Id
     @ManyToOne
-    @JoinColumn(name = "agentID", referencedColumnName = "agentID")
+    @JoinColumn(name = "agentID") // mã đại lý
     private Agent agentID;
 
     @Id
     @ManyToOne
-    @JoinColumn(name = "salesReportID", referencedColumnName = "salesReportID")
+    @JoinColumn(name = "salesReportID") // mã báo cáo doanh số
     private SalesReport salesReportID;
 
-    @Column(name = "quantityExport")
+    @Column(name = "quantityExport")//số phiếu xuất
     private int quantityExport;
 
-    @Column(name = "totalValue")
+    @Column(name = "totalValue")//tổng giá trị
     private int totalValue;
 
-    @Column(name = "proportion")
+    @Column(name = "proportion")//tỷ lệ
     private double proportion;
 
-    public SalesReportDetail() {
+    public SalesReportDetail() 
+    {
+
     }
 
-    public SalesReportDetail(Agent agentID, SalesReport salesReportID, int quantityExport, int totalValue, double proportion) {
+    public SalesReportDetail(Agent agentID, SalesReport salesReportID, int quantityExport, int totalValue, double proportion) 
+    {
         this.agentID = agentID;
         this.salesReportID = salesReportID;
         this.quantityExport = quantityExport;
@@ -38,38 +42,90 @@ public class SalesReportDetail {
         this.proportion = proportion;
     }
 
-    // Getters & Setters...
+    public Agent getAgentID() 
+    {
+        return agentID;
+    }
 
-    public static class SalesReportDetailID implements Serializable {
+    public void setAgentID(Agent agentID) 
+    {
+        this.agentID = agentID;
+    }
+
+    public SalesReport getSalesReportID() 
+    {
+        return salesReportID;
+    }
+
+    public void setSalesReportID(SalesReport salesReportID) 
+    {
+        this.salesReportID = salesReportID;
+    }
+
+    public int getQuantityExport() 
+    {
+        return quantityExport;
+    }
+
+    public void setQuantityExport(int quantityExport) 
+    {
+        this.quantityExport = quantityExport;
+    }
+
+    public int getTotalValue() 
+    {
+        return totalValue;
+    }
+
+    public void setTotalValue(int totalValue) 
+    {
+        this.totalValue = totalValue;
+    }
+
+    public double getProportion() 
+    {
+        return proportion;
+    }
+
+    public void setProportion(double proportion) 
+    {
+        this.proportion = proportion;
+    }
+
+    public static class SalesReportDetailID implements Serializable 
+    {
         private int agentID;
         private int salesReportID;
 
-        public SalesReportDetailID() {
+        public SalesReportDetailID() 
+        {
+
         }
 
-        public SalesReportDetailID(int agentID, int salesReportID) {
+        public SalesReportDetailID(int agentID, int salesReportID) 
+        {
             this.agentID = agentID;
             this.salesReportID = salesReportID;
         }
 
-        // Getters & Setters...
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            SalesReportDetailID that = (SalesReportDetailID) o;
-
-            if (agentID != that.agentID) return false;
-            return salesReportID == that.salesReportID;
+        public int getAgentID() 
+        {
+            return agentID;
         }
 
-        @Override
-        public int hashCode() {
-            int result = agentID;
-            result = 31 * result + salesReportID;
-            return result;
+        public void setAgentID(int agentID) 
+        {
+            this.agentID = agentID;
+        }
+
+        public int getSalesReportID() 
+        {
+            return salesReportID;
+        }
+
+        public void setSalesReportID(int salesReportID) 
+        {
+            this.salesReportID = salesReportID;
         }
     }
 }
