e6e42d6eaba6ac4d1d6176c348e04446d59400e2
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 05:05:27 +0700

    Update DebtReportID.java

--- server/src/main/java/org/example/AgentManagementBE/Model/DebtReportID.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/DebtReportID.java
@@ -2,47 +2,53 @@ package org.example.AgentManagementBE.Model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
-public class DebtReportID implements Serializable 
-{
-    @Column(name = "monthTime") // tháng
+public class DebtReportID implements Serializable {
+
+    @Column(name = "monthTime")
     private int month;
-    @Column(name = "yearTime") // năm
+
+    @Column(name = "yearTime")
     private int year;
-    @ManyToOne
-    @JoinColumn(name = "agentID") // mã đại lý
-    private Agent agentID;
 
+    @Column(name = "agentID")
+    private int agentID;
 
-    public DebtReportID() 
-    {
-
+    public DebtReportID() {
     }
 
-    public DebtReportID(int month, int year, Agent agentID) 
-    {
+    public DebtReportID(int month, int year, int agentID) {
         this.month = month;
         this.year = year;
         this.agentID = agentID;
     }
 
-    public int getMonth() 
-    {
+    public int getMonth() {
         return month;
     }
 
-    public int getYear() 
-    {
+    public int getYear() {
         return year;
     }
 
-    public Agent getAgentID() 
-    {
+    public int getAgentID() {
         return agentID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DebtReportID)) return false;
+        DebtReportID that = (DebtReportID) o;
+        return month == that.month && year == that.year && agentID == that.agentID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(month, year, agentID);
     }
 }
