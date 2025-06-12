92a97ecf3f4a5e8c010db885f4aef9fe0d90767a
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 03:55:35 +0700

    Update ImportDetail.java

--- server/src/main/java/org/example/AgentManagementBE/Model/ImportDetail.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/ImportDetail.java
@@ -1,138 +1,43 @@
-package org.example.AgentManagementBE.Model;
+public static class ImportDetailID implements Serializable {
+    private int importReceiptID;
+    private int productID;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import java.io.Serializable;
+    public ImportDetailID() {}
 
-@Entity
-@IdClass(ImportDetail.ImportDetailID.class)
-public class ImportDetail 
-{
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "importReceiptID")
-    private ImportReceipt importReceiptID;
-
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "productID") // mã sản phẩm
-    private Product productID;
-
-    @Column(name = "quantityImport") // số lượng nhập
-    private int quantityImport;
-
-
-    @Column(name = "importPrice") // giá nhập
-    private int importPrice;
-
-    @Column(name = "intoMoney") // thành tiền
-    private int intoMoney;
-
-    @Column(name = "unit") // đơn vị tính
-    private int unit;
-
-    public ImportDetail() 
-    {
-
-    }
-
-    public ImportDetail(ImportReceipt importReceiptID, Product productID, int quantityImport, int importPrice, int intoMoney, int unit) 
-    {
+    public ImportDetailID(int importReceiptID, int productID) {
         this.importReceiptID = importReceiptID;
         this.productID = productID;
-        this.quantityImport = quantityImport;
-        this.importPrice = importPrice;
-        this.intoMoney = intoMoney;
-        this.unit = unit;
     }
 
-    public ImportReceipt getImportReceiptID() 
-    {
+    public int getImportReceiptID() {
         return importReceiptID;
     }
 
-    public void setImportReceiptID(ImportReceipt importReceiptID) 
-    {
+    public void setImportReceiptID(int importReceiptID) {
         this.importReceiptID = importReceiptID;
     }
 
-    public Product getProductID() 
-    {
+    public int getProductID() {
         return productID;
     }
 
-    public void setProductID(Product productID) 
-    {
+    public void setProductID(int productID) {
         this.productID = productID;
     }
 
-    public int getQuantityImport() {
-        return quantityImport;
+    // ✅ Override equals()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ImportDetailID)) return false;
+        ImportDetailID that = (ImportDetailID) o;
+        return importReceiptID == that.importReceiptID &&
+               productID == that.productID;
     }
 
-    public void setQuantityImport(int quantityImport) 
-    {
-        this.quantityImport = quantityImport;
-    }
-
-    public int getImportPrice() 
-    {
-        return importPrice;
-    }
-
-    public void setImportPrice(int importPrice) 
-    {
-        this.importPrice = importPrice;
-    }
-
-    public int getIntoMoney() 
-    {
-        return intoMoney;
-    }
-
-    public void setIntoMoney(int intoMoney) 
-    {
-        this.intoMoney = intoMoney;
-    }
-
-    public int getUnit() 
-    {
-        return unit;
-    }
-
-    public void setUnit(int unit) 
-    {
-        this.unit = unit;
-    }
-
-    public static class ImportDetailID implements Serializable 
-    {
-        private int importReceiptID;
-        private int productID;
-
-        public ImportDetailID() 
-        {
-
-        }
-
-        public int getImportReceiptID() 
-        {
-            return importReceiptID;
-        }
-
-        public void setImportReceiptID(int importReceiptID) 
-        {
-            this.importReceiptID = importReceiptID;
-        }
-
-        public int getProductID() 
-        {
-            return productID;
-        }
-
-        public void setProductID(int productID) 
-        {
-            this.productID = productID;
-        }
+    // ✅ Override hashCode()
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(importReceiptID, productID);
     }
 }
