c2750a1198676fb83d2f701fa3b20860bd4d9d08
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 03:15:18 +0700

    Update Admin.java

--- server/src/main/java/org/example/AgentManagementBE/Model/Admin.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/Admin.java
@@ -3,40 +3,44 @@ package org.example.AgentManagementBE.Model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "admin") // Khớp tên bảng Flyway đã tạo
-public class Admin {
-
+public class Admin 
+{
     @Id
-    @Column(name = "useremail", nullable = false) // Khớp chính xác tên cột đã tạo qua Flyway
+    @Column(name = "userEmail")
     private String userEmail;
-
-    @Column(name = "password", nullable = false)
+    @Column(name = "password",nullable = false)
     private String password;
 
-    public Admin() {
+    public Admin() 
+    {
+
     }
 
-    public Admin(String userEmail, String password) {
+    public Admin(String userEmail, String password) 
+    {
         this.userEmail = userEmail;
         this.password = password;
     }
 
-    public String getUserEmail() {
+    public String getUserEmail() 
+    {
         return userEmail;
     }
 
-    public void setUserEmail(String userEmail) {
+    public void setUserEmail(String userEmail) 
+    {
         this.userEmail = userEmail;
     }
 
-    public String getPassword() {
+    public String getPassword() 
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) 
+    {
         this.password = password;
     }
 }
