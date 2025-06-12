e220147fd184aefef6921c658ffbad7b4cc37003
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 02:54:47 +0700

    Update DebtReportID.java

--- server/src/main/java/org/example/AgentManagementBE/Model/DebtReportID.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/DebtReportID.java
@@ -4,45 +4,50 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
 import java.io.Serializable;
 
 @Embeddable
-public class DebtReportID implements Serializable 
-{
-    @Column(name = "monthTime") // tháng
+public class DebtReportID implements Serializable {
+
+    @Column(name = "monthTime")
     private int month;
-    @Column(name = "yearTime") // năm
+
+    @Column(name = "yearTime")
     private int year;
+
     @ManyToOne
-    @JoinColumn(name = "agentID") // mã đại lý
+    @JoinColumn(name = "agentID")
     private Agent agentID;
 
+    public DebtReportID() {}
 
-    public DebtReportID() 
-    {
-
-    }
-
-    public DebtReportID(int month, int year, Agent agentID) 
-    {
+    public DebtReportID(int month, int year, Agent agentID) {
         this.month = month;
         this.year = year;
         this.agentID = agentID;
     }
 
-    public int getMonth() 
-    {
+    public int getMonth() {
         return month;
     }
 
-    public int getYear() 
-    {
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
         return year;
     }
 
-    public Agent getAgentID() 
-    {
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Agent getAgentID() {
         return agentID;
+    }
+
+    public void setAgentID(Agent agentID) {
+        this.agentID = agentID;
     }
 }
