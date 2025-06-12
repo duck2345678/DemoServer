4f06d89b162fa277aef00be32ba7a84925ef1772
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 03:07:29 +0700

    Update District.java

--- server/src/main/java/org/example/AgentManagementBE/Model/District.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/District.java
@@ -3,38 +3,48 @@ package org.example.AgentManagementBE.Model;
 import jakarta.persistence.*;
 
 @Entity
-public class District {
+public class District 
+{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "districtID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "districtID") // mã quận
     private int districtID;
-
-    @Column(name = "districtName", nullable = false, columnDefinition = "VARCHAR(255)")
+    @Column(name = "districtName",nullable = false,columnDefinition = "VARCHAR(255)") // tên quận
     private String districtName;
 
-    public District() {}
-
-    public District(int districtID) {
+    public District(int districtID) 
+    {
         this.districtID = districtID;
     }
 
-    public District(String districtName) {
+    public District(String districtName) 
+    {
+
         this.districtName = districtName;
     }
 
-    public int getDistrictID() {
+    public District() 
+    {
+
+    }
+
+    public int getDistrictNO() 
+    {
         return districtID;
     }
 
-    public void setDistrictID(int districtID) {
-        this.districtID = districtID;
+    public void setDistrictNO(Integer districtNO) 
+    {
+        this.districtID = districtNO;
     }
 
-    public String getDistrictName() {
+    public String getDistrictName() 
+    {
         return districtName;
     }
 
-    public void setDistrictName(String districtName) {
+    public void setDistrictName(String districtName) 
+    {
         this.districtName = districtName;
     }
 }
