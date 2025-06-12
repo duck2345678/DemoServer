ac484eb94f2c831232877af04746ec7b3253a812
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 03:16:33 +0700

    Update AgentType.java

--- server/src/main/java/org/example/AgentManagementBE/Model/AgentType.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/AgentType.java
@@ -3,47 +3,56 @@ package org.example.AgentManagementBE.Model;
 import jakarta.persistence.*;
 
 @Entity
-public class AgentType {
+public class AgentType 
+{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "agentTypeID")
     private int agentTypeID;
+    @Column(name = "agentTypeName",nullable = false,columnDefinition = "NVARCHAR(255)")
 
-    @Column(name = "agentTypeName", nullable = false, columnDefinition = "VARCHAR(255)")
     private String agentTypeName;
-
-    @Column(name = "maximumDebt", nullable = false)
+    @Column(name = "maximumDebt",nullable = false) // số tiền nợ tối đa
     private int maximumDebt;
 
-    public AgentType() {
-    }
-
-    public AgentType(String agentTypeName, int maximumDebt) {
+    public AgentType(String agentTypeName, int maximumDebt) 
+    {
         this.agentTypeName = agentTypeName;
         this.maximumDebt = maximumDebt;
     }
 
-    public int getAgentTypeID() {
+    public AgentType() {
+
+    }
+
+
+    public int getAgentTypeID() 
+    {
         return agentTypeID;
     }
 
-    public void setAgentTypeID(int agentTypeID) {
+    public void setAgentTypeID(int agentTypeID) 
+    {
         this.agentTypeID = agentTypeID;
     }
 
-    public String getAgentTypeName() {
+    public String getAgentTypeName() 
+    {
         return agentTypeName;
     }
 
-    public void setAgentTypeName(String agentTypeName) {
+    public void setAgentTypeName(String agentTypeName) 
+    {
         this.agentTypeName = agentTypeName;
     }
 
-    public int getMaximumDebt() {
+    public int getMaximumDebt() 
+    {
         return maximumDebt;
     }
 
-    public void setMaximumDebt(int maximumDebt) {
+    public void setMaximumDebt(int maximumDebt) 
+    {
         this.maximumDebt = maximumDebt;
     }
 }
