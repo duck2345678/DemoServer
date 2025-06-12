edd046c46208bd07b892e4436a57da3ca948f84c
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 01:29:14 +0700

    Update Unit.java

--- server/src/main/java/org/example/AgentManagementBE/Model/Unit.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/Unit.java
@@ -3,42 +3,37 @@ package org.example.AgentManagementBE.Model;
 import jakarta.persistence.*;
 
 @Entity(name = "Unit")
-public class Unit 
-{
+public class Unit {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "unit_seq")
+    @SequenceGenerator(name = "unit_seq", sequenceName = "unit_sequence", allocationSize = 1)
     @Column(name = "unitID") // mã đơn vị tính
     private int unitID;
-    @Column(name = "unitName",nullable = false,columnDefinition = "NVARCHAR(255)") // tên đơn vị tính
+
+    @Column(name = "unitName", nullable = false, length = 255) // tên đơn vị tính
     private String unitName;
 
-    public Unit() 
-    {
-
+    public Unit() {
     }
 
-    public Unit(String unitName) 
-    {
+    public Unit(String unitName) {
         this.unitName = unitName;
     }
 
-    public int getUnitID() 
-    {
+    public int getUnitID() {
         return unitID;
     }
 
-    public void setUnitID(int unitID) 
-    {
+    public void setUnitID(int unitID) {
         this.unitID = unitID;
     }
 
-    public String getUnitName() 
-    {
+    public String getUnitName() {
         return unitName;
     }
 
-    public void setUnitName(String unitName) 
-    {
+    public void setUnitName(String unitName) {
         this.unitName = unitName;
     }
 }
