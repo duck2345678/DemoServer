b1ee09706047006b6e10fcbb5b4d52d647c57cec
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 01:20:13 +0700

    Update ExportReceipt.java

--- server/src/main/java/org/example/AgentManagementBE/Model/ExportReceipt.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/ExportReceipt.java
@@ -3,112 +3,110 @@ package org.example.AgentManagementBE.Model;
 import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class ExportReceipt 
-{
+public class ExportReceipt {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "exportReceiptID") // mã phiếu xuất
+    @Column(name = "exportReceiptID")
     private int exportReceiptID;
 
-    @Column(name = "dateReceipt", nullable = false) // ngày lập phiếu
+    @CreatedDate
+    @Column(name = "dateReceipt", nullable = false, updatable = false)
     private Date dateReceipt;
 
     @ManyToOne
-    @JoinColumn(name = "agentID", nullable = false) // mã đại lý
-    private Agent agentID;
+    @JoinColumn(name = "agentID", nullable = false)
+    private Agent agent;
 
-    @Column(name = "totalMoney") // tổng tiền
+    @Column(name = "totalMoney")
     private int totalMoney;
 
-    @Column(name = "paymentAmount") // số tiền thanh toán
+    @Column(name = "paymentAmount")
     private int paymentAmount;
 
-    @Column(name = "remainAmount") // số tiền còn lại
+    @Column(name = "remainAmount")
     private int remainAmount;
 
+    @OneToMany(mappedBy = "exportReceipt", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ExportDetail> exportDetails = new ArrayList<>();
 
-
-    public ExportReceipt() 
-    {
+    // --- Constructors ---
+    public ExportReceipt() {
     }
 
-    public ExportReceipt(int exportReceiptID) 
-    {
-        this.exportReceiptID = exportReceiptID;
-    }
-
-    public ExportReceipt(Agent agentID, int totalMoney, int paymentAmount, int remainAmount) 
-    {
-        this.agentID = agentID;
+    public ExportReceipt(Agent agent, int totalMoney, int paymentAmount, int remainAmount) {
+        this.agent = agent;
         this.totalMoney = totalMoney;
         this.paymentAmount = paymentAmount;
         this.remainAmount = remainAmount;
     }
 
-    public int getExportReceiptID() 
-    {
+    public ExportReceipt(int exportReceiptID) {
+        this.exportReceiptID = exportReceiptID;
+    }
+
+    // --- Getters & Setters ---
+
+    public int getExportReceiptID() {
         return exportReceiptID;
     }
 
-    public void setExportReceiptID(int exportReceiptID) 
-    {
+    public void setExportReceiptID(int exportReceiptID) {
         this.exportReceiptID = exportReceiptID;
     }
 
-    public Date getDateReceipt() 
-    {
+    public Date getDateReceipt() {
         return dateReceipt;
     }
 
-    public void setDateReceipt(Date dateReceipt) 
-    {
+    public void setDateReceipt(Date dateReceipt) {
         this.dateReceipt = dateReceipt;
     }
 
-    public Agent getAgentID() {
-        return agentID;
+    public Agent getAgent() {
+        return agent;
     }
 
-    public void setAgentID(Agent agentID) 
-    {
-        this.agentID = agentID;
+    public void setAgent(Agent agent) {
+        this.agent = agent;
     }
 
-    public int getTotalMoney() 
-    {
+    public int getTotalMoney() {
         return totalMoney;
     }
 
-    public void setTotalMoney(int totalMoney) 
-    {
+    public void setTotalMoney(int totalMoney) {
         this.totalMoney = totalMoney;
     }
 
-    public int getPaymentAmount() 
-    {
+    public int getPaymentAmount() {
         return paymentAmount;
     }
 
-    public void setPaymentAmount(int paymentAmount) 
-    {
+    public void setPaymentAmount(int paymentAmount) {
         this.paymentAmount = paymentAmount;
     }
 
-    public int getRemainAmount() 
-    {
+    public int getRemainAmount() {
         return remainAmount;
     }
 
-    public void setRemainAmount(int remainAmount) 
-    {
+    public void setRemainAmount(int remainAmount) {
         this.remainAmount = remainAmount;
     }
 
-  
+    public List<ExportDetail> getExportDetails() {
+        return exportDetails;
+    }
+
+    public void setExportDetails(List<ExportDetail> exportDetails) {
+        this.exportDetails = exportDetails;
+    }
 }
