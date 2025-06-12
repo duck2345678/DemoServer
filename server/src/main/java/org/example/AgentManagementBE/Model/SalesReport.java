f4585477098abeb459693778727df6a0c6b868da
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 01:27:32 +0700

    Update SalesReport.java

--- server/src/main/java/org/example/AgentManagementBE/Model/SalesReport.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/SalesReport.java
@@ -3,77 +3,66 @@ package org.example.AgentManagementBE.Model;
 import jakarta.persistence.*;
 
 @Entity
-public class SalesReport 
-{
+public class SalesReport {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "salesReportID") // mã báo cáo doanh số
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sales_report_seq")
+    @SequenceGenerator(name = "sales_report_seq", sequenceName = "sales_report_sequence", allocationSize = 1)
+    @Column(name = "salesReportID")
     private int salesReportID;
 
-    @Column(name = "monthtime",nullable = false) // tháng
+    @Column(name = "monthtime", nullable = false)
     private int month;
 
-    @Column(name = "yeartime",nullable = false) // năm
+    @Column(name = "yeartime", nullable = false)
     private int year;
 
-    @Column(name = "totalRevenue") // tổng doanh số
+    @Column(name = "totalRevenue")
     private int totalRevenue;
 
-    public SalesReport() 
-    {
-        
+    public SalesReport() {
     }
 
-    public SalesReport(int month, int year, int totalRevenue) 
-    {
+    public SalesReport(int month, int year, int totalRevenue) {
         this.month = month;
         this.year = year;
         this.totalRevenue = totalRevenue;
     }
 
-    public SalesReport(int month, int year) 
-    {
+    public SalesReport(int month, int year) {
         this.month = month;
         this.year = year;
     }
 
-    public int getSalesReportID() 
-    {
+    public int getSalesReportID() {
         return salesReportID;
     }
 
-    public void setSalesReportID(int salesReportID) 
-    {
+    public void setSalesReportID(int salesReportID) {
         this.salesReportID = salesReportID;
     }
 
-    public int getMonth() 
-    {
+    public int getMonth() {
         return month;
     }
 
-    public void setMonth(int month) 
-    {
+    public void setMonth(int month) {
         this.month = month;
     }
 
-    public int getYear() 
-    {
+    public int getYear() {
         return year;
     }
 
-    public void setYear(int year) 
-    {
+    public void setYear(int year) {
         this.year = year;
     }
 
-    public int getTotalRevenue() 
-    {
+    public int getTotalRevenue() {
         return totalRevenue;
     }
 
-    public void setTotalRevenue(int totalRevenue) 
-    {
+    public void setTotalRevenue(int totalRevenue) {
         this.totalRevenue = totalRevenue;
     }
 }
