fed7ca764e287936a6084238acd5e890e65bb35f
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 01:38:25 +0700

    Update DebtReportID.java

--- server/src/main/java/org/example/AgentManagementBE/Model/DebtReportID.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/DebtReportID.java
@@ -1,41 +1,48 @@
+package org.example.AgentManagementBE.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
 @Embeddable
-public class DebtReportID implements Serializable {
-
-    @Column(name = "monthTime")
+public class DebtReportID implements Serializable 
+{
+    @Column(name = "monthTime") // tháng
     private int month;
-
-    @Column(name = "yearTime")
+    @Column(name = "yearTime") // năm
     private int year;
+    @ManyToOne
+    @JoinColumn(name = "agentID") // mã đại lý
+    private Agent agentID;
 
-    @Column(name = "agentID")
-    private int agentID;
 
-    public DebtReportID() {}
+    public DebtReportID() 
+    {
 
-    public DebtReportID(int month, int year, int agentID) {
+    }
+
+    public DebtReportID(int month, int year, Agent agentID) 
+    {
         this.month = month;
         this.year = year;
         this.agentID = agentID;
     }
 
-    public int getMonth() { return month; }
-
-    public int getYear() { return year; }
-
-    public int getAgentID() { return agentID; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof DebtReportID)) return false;
-        DebtReportID that = (DebtReportID) o;
-        return month == that.month &&
-               year == that.year &&
-               agentID == that.agentID;
+    public int getMonth() 
+    {
+        return month;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(month, year, agentID);
+    public int getYear() 
+    {
+        return year;
+    }
+
+    public Agent getAgentID() 
+    {
+        return agentID;
     }
 }
