5125efb847c6f5ee09065c213603c2b41d98e4ed
duck2345678 <23520311@gm.uit.edu.vn>
2025-06-13 05:14:38 +0700

    Update DebtReportID.java

--- server/src/main/java/org/example/AgentManagementBE/Model/DebtReportID.java
+++ server/src/main/java/org/example/AgentManagementBE/Model/DebtReportID.java
@@ -1,54 +1,48 @@
 package org.example.AgentManagementBE.Model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Embeddable
 public class DebtReportID implements Serializable {
-
-    @Column(name = "monthTime")
     private int month;
-
-    @Column(name = "yearTime")
     private int year;
 
-    @Column(name = "agentID")
-    private int agentID;
+    @ManyToOne
+    @JoinColumn(name = "agent_id")
+    private Agent agent;
 
-    public DebtReportID() {
-    }
+    public DebtReportID() {}
 
-    public DebtReportID(int month, int year, int agentID) {
+    public DebtReportID(int month, int year, Agent agent) {
         this.month = month;
         this.year = year;
-        this.agentID = agentID;
+        this.agent = agent;
     }
 
-    public int getMonth() {
-        return month;
-    }
+    // Getters and Setters
+    public int getMonth() { return month; }
+    public void setMonth(int month) { this.month = month; }
 
-    public int getYear() {
-        return year;
-    }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
 
-    public int getAgentID() {
-        return agentID;
-    }
+    public Agent getAgent() { return agent; }
+    public void setAgent(Agent agent) { this.agent = agent; }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof DebtReportID)) return false;
         DebtReportID that = (DebtReportID) o;
-        return month == that.month && year == that.year && agentID == that.agentID;
+        return month == that.month &&
+               year == that.year &&
+               Objects.equals(agent.getAgentID(), that.agent.getAgentID());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(month, year, agentID);
+        return Objects.hash(month, year, agent.getAgentID());
     }
 }
